03e19ddf8bf7e1ed9106f02313969854ed8a2797
Madan <58905767+Madanaa@users.noreply.github.com>
2021-07-15 01:44:31 -0700

    Update TestCalculator.java
    Check with other return type value

--- src/calculator/co/TestCalculator.java
+++ src/calculator/co/TestCalculator.java
@@ -14,18 +14,23 @@ public class TestCalculator {
 	@Test
     public void oneNumberShouldReturnItseft() {
         Calculator cal = new Calculator();
-        assertEquals(0, cal.calculate("1"));
+        assertEquals(1, cal.calculate("1"));
     }
 	@Test
     public void twoNumbersShouldBeAdded() {
         Calculator cal = new Calculator();
-        assertEquals(0, cal.calculate("1,2"));
+        assertEquals(1+2, cal.calculate("1,2"));
     }
 	@Test
     public void moreDigitsSupported() {
         Calculator cal = new Calculator();
 
-        assertEquals(1, cal.calculate("15,30"));
+        assertEquals(15*30, cal.calculate("15,30"));
+    }
+	@Test
+    public void allowNnumersAsInput() {
+        Calculator cal = new Calculator();
+        assertEquals(1+2+3, cal.calculate("1,2,3"));
     }
     
 }
